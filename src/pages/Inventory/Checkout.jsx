9f42c60e4c0efae50a723ad49bc09035143313d7
JC <jctanmaquera@gmail.com>
2025-11-19 11:05:09 +0400

    fix import InventoryTabs

--- src/pages/Inventory/Checkout.jsx
+++ src/pages/Inventory/Checkout.jsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ReceiptText, ScanLine, Trash2 } from "lucide-react";
-import InventoryTabs from "./InventoryTabs";
+import InventoryTabs from "@/components/Inventory/InventoryTabs";
 import {
   Dialog,
   DialogContent,
